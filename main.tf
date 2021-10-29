d7da0dcb2d733fc955f4ddfa05dc0d21bf21862a
Victor <victorvmaciel@gmail.com>
2021-10-29 14:47:36 -0300

    Add ansible files

--- main.tf
+++ main.tf
@@ -12,3 +12,13 @@ provider "aws" {
   region = "us-west-2"
 }
 
+resource "aws_instance" "app_server" {
+  ami = "ami-03d5c68bab01f3496"
+  instance_type = "t2.micro"
+  key_name = "oregon_terraform_key"
+  # user_data = 
+  tags=  {
+    Name = "Terraform | Ansible | AWS"
+
+  }
+ }
